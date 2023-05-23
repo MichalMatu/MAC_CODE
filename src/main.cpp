7e0d3556e8979304b532a4437e8c25de009a0ff2
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-05-23 02:15:49 +0200

    code to mac

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
-#include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void shortStringToMac(String shortString, uint8_t mac[])
+{
+  String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Convert short string to MAC address
+  unsigned long long macValue = 0;
+  for (char c : shortString)
+  {
+    macValue *= 36;
+    macValue += characters.indexOf(c);
+  }
+
+  for (int i = 5; i >= 0; i--)
+  {
+    mac[i] = macValue & 0xFF;
+    macValue >>= 8;
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(115200);
+
+  // Set your desired short MAC address here
+  String shortString = "2QIY9I";
+
+  // Convert short MAC address back to original MAC address
+  uint8_t mac[6];
+  shortStringToMac(shortString, mac);
+
+  // Print the original MAC address
+  Serial.print("Original MAC Address: ");
+  for (int i = 0; i < 6; i++)
+  {
+    Serial.print(mac[i], HEX);
+    if (i < 5)
+    {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // Your code here
 }
